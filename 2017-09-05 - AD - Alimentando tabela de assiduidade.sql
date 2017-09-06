413e7fb3cd65fdcc340fa46b76fc8c612a80424e
Luiza Senna <eu@luizasenna.com>
2017-09-06 11:06:43 -0300

    Update 2017-09-05 - AD - Alimentando tabela de assiduidade.sql

--- 2017-09-05 - AD - Alimentando tabela de assiduidade.sql
+++ 2017-09-05 - AD - Alimentando tabela de assiduidade.sql
@@ -4,3 +4,11 @@ from atrasos
 INNER JOIN funcionarios as f on f.CHAPA = atrasos.chapa
 group by CODPESSOA, YEAR(data), MONTH(data)
 having ANO = 2016;
+
+
+select * from assiduidade
+inner join avaliacoes on year(avaliacoes.DATAABERTURA) = ano and month(avaliacoes.DATAABERTURA)=mes;
+
+update assiduidade 
+inner join avaliacoes on year(avaliacoes.DATAABERTURA) = ano and month(avaliacoes.DATAABERTURA)=mes
+set assiduidade.codavaliacao = avaliacoes.CODAVALIACAO
